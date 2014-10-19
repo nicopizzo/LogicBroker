808f3a090aa2e9d823df91acae48b663f660cba8
nicopizzo <funkiernails@yahoo.com>
2014-10-19 19:44:15 -0400

    Order Details Prototype screen

--- www/js/select_orders.js
+++ www/js/select_orders.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-	$('.tabs .tabsLinks a').on('click', function(e) {
-		var currentAttrValue = $(this).attr('href');
-		
-		$('.tabs ' + currentAttrValue).show().siblings().hide();
-		
-		$(this).parent('li').addClass('active').siblings().removeClass('active');
-		
-		e.preventDefault();
-	});
+	$("body").css("background-color","#ffff212");
 });
