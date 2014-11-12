9525d1b7c425544ebddff2bd558fcf46ac47de18
nicopizzo <funkiernails@yahoo.com>
2014-11-12 00:16:04 -0500

    added more drag and drop functionality
    Containers are added dynamically. Just need to do same with cases. Also
    when case is removed, put all items back into the unsorted list.

--- www/js/packingscreen.js
+++ www/js/packingscreen.js
@@ -1,4 +1,26 @@
-$(function () {
+var containerCount = 1;
+var caseCount = 1;
+
+$(document).ready(function() {
+	var colmain = $('#collapsibleSet');
+	var collapsible = $('<div data-role="collapsible" data-collapsed="false" id="container' + containerCount +'">');
+	collapsible.append('<h4>Container 1</h4>');
+	collapsible.append('<button type="button" data-mini="true" data-inline="true" class="addCaseButton" id="addCaseButton' + containerCount + '" onclick="addCaseEvent('+containerCount+')">Add Case</button>');
+	collapsible.append('<button type="button" data-mini="true" data-inline="true" class="removeContainerButton" onclick="removeContainerEvent()">Remove Container</button>');
+
+	var caseSet = $('<div data-role="collapsible-set" class="caseCollapsibleSet" id="caseCollapsibleSet' + containerCount + '">');
+	var case1 = $('<div data-role="collapsible" data-collapsed="false" id="case' + caseCount + '">');
+	case1.append('<h4>Case-Box ' + caseCount + '</h4>');
+	case1.append('<button type="button" data-mini="true" data-inline="true" class="removeCaseButton" onclick="removeCaseEvent()">Remove Case</button>');
+	case1.append('<ul data-role="listview" data-inset="true" data-theme="d" class="draganddrop"><li>LB-04213q</li></ul>');
+	caseSet.append(case1);
+	collapsible.append(caseSet);
+	colmain.append(collapsible);
+    colmain.trigger('create');
+	refreshDragandDrop();
+});
+
+function refreshDragandDrop() {
 	var oldList, newList, item;
 	$(".draganddrop").sortable({
 		connectWith: $('.draganddrop'),
@@ -13,33 +35,55 @@ $(function () {
 			},
 		})
 	.disableSelection();
-});		
-			
-$(function() {
+	
 	$('.draganddrop').css("padding-bottom", "3%");
-});
+}
 
-$(function() {
-	$('.addContainerButton').on("click",function() {
-		 $(this).parent().append(
-			"<div data-role=\"collapsible\" data-collapsed=\"false\">\
-				<h4>Container</h4>\
-				<a href=\"#\" data-role=\"button\" data-inline=\"true\" data-mini=\"true\" class=\"addCaseButton\">Add Case</a>\
-                <a href=\"#\" data-role=\"button\" data-inline=\"true\" data-mini=\"true\" class=\"removeContainerButton\">Remove Container</a>\
-			</div>"
-		 );
-		 
-	});
-});
+function addContainerEvent() {
+		containerCount++;
+		var colmain = $('#collapsibleSet');
+		var collapsible = $('<div data-role="collapsible" data-collapsed="false" id="container' + containerCount + '">');
+		collapsible.append('<h2>Container ' + containerCount + '</h2>');
+		collapsible.append('<button type="button" data-mini="true" data-inline="true" class="addCaseButton" id="addCaseButton' + containerCount + '" onclick="addCaseEvent()">Add Case</button>');
+		collapsible.append('<button type="button" data-mini="true" data-inline="true" class="removeContainerButton" onclick="removeContainerEvent()">Remove Container</button>');
+		
+		var caseSet = $('<div data-role="collapsible-set" id="caseCollapsibleSet' + containerCount + '" class="caseCollapsibleSet">');
+		caseCount++;
+		var case1 = $('<div data-role="collapsible" data-collapsed="false" id="caseSet' + caseCount + '">');
+		case1.append('<h4>Case-Box ' + caseCount + '</h4>');
+		case1.append('<button type="button" data-mini="true" data-inline="true" class="removeCaseButton" onclick="removeCaseEvent()">Remove Case</button>');
+		case1.append('<ul data-role="listview" data-inset="true" data-theme="d" class="draganddrop"><li>LB-04213q</li></ul>');
+		caseSet.append(case1);
+		collapsible.append(caseSet);
+		colmain.append(collapsible);
+		colmain.trigger('create');
+		refreshDragandDrop();
+}
 
-$(function() {
+function addCaseEvent(containerId) {
+		caseCount++;
+		var caseSet = $('#container' + containerId + '.caseCollapsibleSet');
+		console.log(caseSet);
+		var case1 = $('<div data-role="collapsible" data-collapsed="false" id="case' + caseCount + '">');
+		console.log(case1);
+		case1.append('<h4>Case-Box ' + caseCount + '</h4>');
+		case1.append('<button type="button" data-mini="true" data-inline="true" class="removeCaseButton" onclick="removeCaseEvent()">Remove Case</button>');
+		case1.append('<ul data-role="listview" data-inset="true" data-theme="d" class="draganddrop"><li>LB-04213q</li></ul>');
+		console.log(case1);
+		caseSet.append(case1);
+		console.log(caseSet);
+		$('#container' + containerId).trigger('create');
+		refreshDragandDrop();
+}
+
+function removeCaseEvent() {
 	$('.removeCaseButton').click(function() {
 		$(this).parent().parent().remove();
 	});
-});
-
-$(function() {
+}
+	
+function removeContainerEvent() {	
 	$('.removeContainerButton').click(function() {
 		$(this).parent().parent().remove();
 	});
-});
+}
