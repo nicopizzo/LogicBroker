b49eca36b8546c60105c6ba6a6294329f10d7356
nicopizzo <funkiernails@yahoo.com>
2014-11-16 18:44:44 -0500

    applied css to index

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(document).ready(function() {
+	$("#logo").css("display", "block");
+	$("#logo").css("margin-left", "auto");
+	$("#logo").css("margin-right", "auto");
+	$("#logo").css("height", "auto");
+	$("#logo").css("width", "auto");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+	$("#welcomeText").css("text-align", "center");
+	
+	$("#user").css("margin-left", "10%");
+	$("#user").css("margin-right", "10%");
+	$("#pass").css("margin-left", "10%");
+	$("#pass").css("margin-right", "10%");
+	
+	$("#submitButton").css("margin-left", "20%");
+	$("#submitButton").css("margin-right", "20%");
+	
 
-        console.log('Received Event: ' + id);
-    }
-};
+});
